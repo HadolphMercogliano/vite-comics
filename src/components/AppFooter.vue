14fc34041d391cfd0e8a319582a9180f49a6f180
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-27 19:22:52 +0100

    footer html end

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,30 +7,63 @@ export default {};
     <div class="footer-top">
       <div class="container">
         <div class="footer-text">
-          <h3>DC COMICS</h3>
-          <ul>
-            <li>
-              <a href="#">Characters</a>
-            </li>
-            <li>
-              <a href="#">Comics</a>
-            </li>
-            <li>
-              <a href="#">Movies</a>
-            </li>
-            <li>
-              <a href="#">TV</a>
-            </li>
-            <li>
-              <a href="#">Games</a>
-            </li>
-            <li>
-              <a href="#">Videos</a>
-            </li>
-            <li>
-              <a href="#">News</a>
-            </li>
-          </ul>
+          <div>
+            <h3>DC COMICS</h3>
+            <ul>
+              <li>
+                <a href="#">Characters</a>
+              </li>
+              <li>
+                <a href="#">Comics</a>
+              </li>
+              <li>
+                <a href="#">Movies</a>
+              </li>
+              <li>
+                <a href="#">TV</a>
+              </li>
+              <li>
+                <a href="#">Games</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+            </ul>
+            <h3>SHOP</h3>
+            <ul>
+              <li><a href="#">Shop DC </a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3>DC</h3>
+            <ul>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Privacy Policy (New)</a></li>
+              <li><a href="#"> Ad Choises</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent Workshops</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3>SITES</h3>
+            <ul>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </div>
         </div>
         <div class="bg-logo">
           <img
@@ -90,8 +123,15 @@ export default {};
 <style lang="scss" scoped>
 .footer-top {
   background-image: url(../assets/footer-bg.jpg);
-  //   height: 500px;
   .footer-text {
+    padding: 10px 0;
+    div {
+      margin-bottom: 10px;
+    }
+    h3 {
+      color: white;
+      margin-bottom: 10px;
+    }
     li {
       padding: 2px 0;
       font-size: 0.7rem;
