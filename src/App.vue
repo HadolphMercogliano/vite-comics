348ddd610b7d9735b9eb11c16010d9be82c5878b
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-27 17:54:46 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  components: { AppHeader, AppFooter },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppFooter />
+</template>
+
+<style lang="scss"></style>
