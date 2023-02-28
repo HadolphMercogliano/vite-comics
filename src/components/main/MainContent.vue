5cba56febbc1ec6f3a616967ff0eaec09787358d
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-28 19:23:18 +0100

    main content structure

---
+++ src/components/main/MainContent.vue
@@ -0,0 +1,29 @@
+<script>
+//import componenti
+import Jumbotron from "./jumbotron.vue";
+import MainFooter from "./MainFooter.vue";
+
+export default {
+  //dichiarazione
+  components: { MainFooter, Jumbotron },
+};
+</script>
+<template>
+  <div>
+    <Jumbotron />
+    <div class="cardContainer">
+      <div>
+        <button></button>
+      </div>
+    </div>
+    <MainFooter />
+  </div>
+</template>
+<style lang="scss" scoped>
+@use "../../assets/styles/mixins.scss" as *;
+@use "../../assets/styles/variables.scss" as *;
+.cardContainer {
+  button {
+  }
+}
+</style>
